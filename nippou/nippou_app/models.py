0c56d3377c6be3c62d1ee2a2c0c13d409fe7c047
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-05-25 15:36:14 +0900

    confirm migration and view

--- nippou/nippou_app/models.py
+++ nippou/nippou_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Nippou(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    owner = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
